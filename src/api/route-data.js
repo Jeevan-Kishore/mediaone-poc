f561c75e1cb5b4797f90c39a2cad8ff135bdb5d6
jeevan-kishore <kishor080@gmail.com>
2020-12-21 18:36:38 +0530

    fix(fetch): Fix node fetch :hammer:

--- src/api/route-data.js
+++ src/api/route-data.js
@@ -1,6 +1,8 @@
+const fetch = require('node-fetch');
+
 module.exports = async (req, res) => {
     const { path = '/' } = req.query;
-    const response = await (await fetch(`https://www.mediaonetv.in/route-data.json?path=${path}`)).json();
+    const response = await fetch(`https://www.mediaonetv.in/route-data.json?path=${path}`);
     await res.json({
         body: response
     })

50bfcf0a19759da585e1fffd1722d9e9df1deb6d
jeevan-kishore <kishor080@gmail.com>
2020-12-22 11:05:48 +0530

    fix(api): Lazy load youtube

--- src/components/Youtube.svelte
+++ src/components/Youtube.svelte
@@ -1,7 +1,15 @@
 <script>
+    import IntersectionObserver from './Image/IntersectionObserver.svelte'
     export let youtubeUrl = "";
 </script>
 
-<iframe title="youtube-video" id="ytplayer" type="text/html" width="640" height="360"
-        src={youtubeUrl}
-        frameborder="0"></iframe>
+<IntersectionObserver once={true} let:intersecting={intersecting}>
+    {#if intersecting}
+        <iframe title="youtube-video" id="ytplayer" type="text/html" width="640" height="360"
+                src={youtubeUrl}
+                frameborder="0"></iframe>
+    {/if}
+</IntersectionObserver>
+
+
+
